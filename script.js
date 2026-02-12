2e0ff60c8070fa7b82ed2ffa81edb7c92b51b13b
Cyuuovo <doubleyan1016@gmail.com>
2026-02-12 22:45:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -77,8 +77,11 @@ const now = () => Date.now();
 
 function formatToday(){
   const d = new Date();
-  const ws = ['日','一','二','三','四','五','六'];
-  return `${d.getMonth()+1}月${d.getDate()}日 周${ws[d.getDay()]}`;
+  const ws = ['周日','周一','周二','周三','周四','周五','周六'];
+
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  const dd = String(d.getDate()).padStart(2, '0');
+  return `${mm}/${dd} ${ws[d.getDay()]}`;
 }
 
 function updateDate(){
